443730b4068b72f27cd0b1f5615f7a4a67325a06
Lele Chen <lelechen@dhcp-10-5-14-170.wireless.rochester.edu>
2022-01-23 11:07:19 -0500

    g

--- scripts/test.py
+++ scripts/test.py
@@ -54,8 +54,9 @@ class CIPS_3D_Demo(object):
     seed = 1
     # trajectory
     trajectory_mode = st_utils.selectbox('trajectory_mode', cfg.trajectory_mode, sidebar=True)
-    print (type(trajectory_mode))
-    print (trajectory_mode)
+    # print (type(trajectory_mode))
+    # print (trajectory_mode)
+    trajectory_mode = 'yaw'
     forward_points = st_utils.number_input('forward_points', cfg.forward_points, sidebar=True)
 
     # ****************************************************************************
