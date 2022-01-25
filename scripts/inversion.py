be8a420182887e437ad3c033d0a71646c7b77061
Lele Chen <lelechen@dhcp-10-5-14-170.wireless.rochester.edu>
2022-01-24 19:50:38 -0500

    g

--- scripts/inversion.py
+++ scripts/inversion.py
@@ -147,6 +147,7 @@ class CIPS_3D_Demo(object):
         synth_features = vgg16(synth_images, resize_images=False, return_lpips=True)
         dist = (target_features - synth_features).square().sum()      
         l1 = (target_images - synth_images).square().sum()  
+        print (target_images.max(), target_images.min(),synth_images.max(), synth_images.min(),'+++++++' )
         
         reg_loss = zs['z_nerf'].mean()**2
         reg_loss += zs['z_inr'].mean()**2
