8586a0855758335e50c6ee72016ed9b0483d8ff1
Lele Chen <lelechen@dhcp-10-5-14-170.wireless.rochester.edu>
2022-01-25 22:30:15 -0500

    g

--- scripts/inversion.py
+++ scripts/inversion.py
@@ -150,8 +150,7 @@ class CIPS_3D_Demo(object):
             grad_points = forward_points ** 2,
             camera_lookup=cur_camera_lookup,
             **curriculum)
-        print (synth_images.requires_grad,'=---------')
-        print (gg)
+        
         l1 = l1loss(synth_images, target_images)
         # Downsample image to 256x256 if it's larger than that. VGG was built for 224x224 images.
         # synth_images = (synth_images + 1) * (255/2)
