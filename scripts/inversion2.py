f9ba0f6c830d49b30a9bbc17f743b5013147ffd8
Lele Chen <lelechen@dhcp-10-5-14-170.wireless.rochester.edu>
2022-01-25 21:31:34 -0500

    g

--- scripts/inversion2.py
+++ scripts/inversion2.py
@@ -37,8 +37,8 @@ import torch.distributed as dist
 
 
 def setup_ddp(rank, world_size, port):
-  os.environ['MASTER_ADDR'] = 'localhost'
-  os.environ['MASTER_PORT'] = port
+#   os.environ['MASTER_ADDR'] = 'localhost'
+#   os.environ['MASTER_PORT'] = port
 
   # initialize the process group
   # dist.init_process_group("gloo", rank=rank, world_size=world_size)
