19b57bcea33b8b72f52d1cf01cd412e0b15619cb
Lele Chen <lelechen@Innopeaks-MacBook-Pro.local>
2022-01-26 22:03:13 -0500

    g

--- photometric_optimization/debug.py
+++ photometric_optimization/debug.py
@@ -2,6 +2,6 @@ import os
 import numpy as np
 import pickle
 
-file = open("'/home/uss00022/lelechen/github/CIPS-3D/photometric_optimization/gg/flame_p.pickle'",'r')
+file = open("/home/uss00022/lelechen/github/CIPS-3D/photometric_optimization/gg/flame_p.pickle",'r')
 flame_p = pickle.load(file)
 print (flame_p)
