8da751a6e0faa8474579956f5552e6d6b36cf03b
Lele Chen <lelechen@Innopeaks-MacBook-Pro.local>
2022-01-29 10:23:47 -0500

    g

--- rignet/debug.py
+++ rignet/debug.py
@@ -2,16 +2,16 @@ import numpy as np
 import os 
 import pickle
 
-single_params = {
-            'shape': shape.detach().cpu().numpy(),
-            'exp': exp.detach().cpu().numpy(),
-            'pose': pose.detach().cpu().numpy(),
-            'cam': cam.detach().cpu().numpy(),
-            'verts': trans_vertices.detach().cpu().numpy(),
-            'albedos':albedos.detach().cpu().numpy(),
-            'tex': tex.detach().cpu().numpy(),
-            'lit': lights.detach().cpu().numpy()
-        }
+# single_params = {
+#             'shape': shape.detach().cpu().numpy(),
+#             'exp': exp.detach().cpu().numpy(),
+#             'pose': pose.detach().cpu().numpy(),
+#             'cam': cam.detach().cpu().numpy(),
+#             'verts': trans_vertices.detach().cpu().numpy(),
+#             'albedos':albedos.detach().cpu().numpy(),
+#             'tex': tex.detach().cpu().numpy(),
+#             'lit': lights.detach().cpu().numpy()
+#         }
 
 
 with open("/home/uss00022/lelechen/github/CIPS-3D/photometric_optimization/gg/flame_p.pickle", 'rb') as f:
