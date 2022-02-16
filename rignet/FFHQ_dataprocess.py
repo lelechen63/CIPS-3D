ba28ac1984bf3369fb849591887cb249708b9414
Lele Chen <lelechen@Innopeaks-MacBook-Pro.local>
2022-02-15 16:26:19 -0800

    add debug

--- rignet/FFHQ_dataprocess.py
+++ rignet/FFHQ_dataprocess.py
@@ -9,7 +9,6 @@ import cv2
 def debug_single():
     with open("/home/uss00022/lelechen/github/CIPS-3D/photometric_optimization/gg/flame_p.pickle", 'rb') as f:
         flame_p = pickle.load(f, encoding='latin1')
-    print (flame_p)
 
     for key in flame_p.keys():
         print ('=-------------')
