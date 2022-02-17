1329b3ed8bbe7bee1228687d0bb20827bba4cf0e
Lele Chen <lelechen@Innopeaks-MacBook-Pro-2.local>
2022-02-17 09:45:40 -0800

    add debug

--- rignet/rignet.py
+++ rignet/rignet.py
@@ -136,7 +136,7 @@ class Latent2CodeModule(pl.LightningModule):
         landmarks2d, predicted_images = self(batch['shape_latent'], batch['appearance_latent'], batch['cam'], batch['pose'])
 
         losses = {}
-        losses['landmark'] = util.l2_distance(landmarks2d[:, 17:, :2], gt_landmarks[:, 17:, :2]) * self.flame_config.w_lmks
+        losses['landmark'] = util.l2_distance(landmarks2d[:, 17:, :2], batch['gt_landmarks'][:, 17:, :2]) * self.flame_config.w_lmks
         losses['photometric_texture'] = (image_masks * (predicted_images - gt_images).abs()).mean() * self.flame.w_pho
 
         all_loss = 0.
