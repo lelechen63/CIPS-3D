db40d4831c386c06bb2edf279f3e7e9f37499263
Lele Chen <lelechen@Innopeaks-MacBook-Pro-2.local>
2022-02-22 14:05:17 -0800

    add debug

--- exp/dev/nerf_inr/models/generator_nerf_inr_v16.py
+++ exp/dev/nerf_inr/models/generator_nerf_inr_v16.py
@@ -1749,15 +1749,17 @@ class GeneratorNerfINR(GeneratorNerfINR_base):
                 shape,
                 device,
                 dist='gaussian'):
-    print ('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    # print ('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
     # if dist == 'gaussian':
-    # z = torch.randn(shape, device=device) 
-    z = 0.02 * truncnorm.rvs(-2, 2, size=shape, random_state=np.random.RandomState(0))
+    z = torch.randn(shape, device=device) 
+    
+    # z = 0.02 * truncnorm.rvs(-2, 2, size=shape, random_state=np.random.RandomState(0))
+    # z= torch.FloatTensor(z).to(device)
     # elif dist == 'uniform':
     #   z = torch.rand(shape, device=device) * 2 - 1
-    # print (z.shape)
+    print (z.max(),z.min())
     # print (type(z))
-    z= torch.FloatTensor(z).to(device)
+    
   
     return z
 
