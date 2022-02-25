20d45cf29145295c4b68297fc72b7a901a8f332e
Lele Chen <lelechen@Innopeaks-MacBook-Pro-2.local>
2022-02-24 17:55:00 -0800

    add debug

--- photometric_optimization/models/lbs.py
+++ photometric_optimization/models/lbs.py
@@ -347,9 +347,10 @@ def batch_rigid_transform(rot_mats, joints, parents, dtype=torch.float32):
 
     rel_joints = joints.clone()
     rel_joints[:, 1:] -= joints[:, parents[1:]]
-
+    rel_joints = rel_joints.contiguous()
     print(rot_mats.shape, '++++++++++++++')
     print(rel_joints.shape, '+++----------++++')
+
     rel_joints =  rel_joints.view(rel_joints.shape[0] * rel_joints.shape[1], 3, 1)
     print(rot_mats.shape, '+++++++22222+++++++')
     print(rel_joints.shape, '+++-----2222-----++++')
