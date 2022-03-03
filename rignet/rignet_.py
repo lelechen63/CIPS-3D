3c74eb3498a50d58a9549b6c0747df7c85f6f1a2
Lele Chen <lelechen@Innopeaks-MacBook-Pro-2.local>
2022-03-02 17:02:12 -0800

    remove

--- rignet/rignet_.py
+++ rignet/rignet_.py
@@ -39,14 +39,12 @@ class Latent2CodeModule():
         for p in self.latent2code.parameters():
             p.requires_grad = True 
         for epoch in range( 1000):
-            print (epoch)
             for step, batch in enumerate(tqdm(self.data_loader)):
-                print (step)
 
                 landmarks3d, predicted_images = self.latent2code(batch['shape_latent'].to(self.device), \
                                             batch['appearance_latent'].to(self.device), \
                                             batch['cam'].to(self.device), batch['pose'].to(self.device))
-
+                print (batch['gt_landmark'].shape )
                 losses = {}
                 losses['landmark'] = util.l2_distance(landmarks3d[:, 17:, :2], batch['gt_landmark'][:, 17:, :2].to(self.device)) * self.flame_config.w_lmks
                 losses['photometric_texture'] = (batch['img_mask'].to(self.device) * (predicted_images - batch['gt_image'].to(self.device) ).abs()).mean() * self.flame_config.w_pho
