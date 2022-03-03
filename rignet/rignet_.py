cd2bfd487eb1a240566d389c9f2953891a39cb28
Lele Chen <lelechen@Innopeaks-MacBook-Pro-2.local>
2022-03-02 16:39:36 -0800

    remove

--- rignet/rignet_.py
+++ rignet/rignet_.py
@@ -37,7 +37,9 @@ class Latent2CodeModule():
         for p in self.latent2code.parameters():
             p.requires_grad = True 
         for epoch in range( 1000):
+            print (epoch)
             for step, batch in enumerate(tqdm(self.data_loader)):
+                print (step)
                 landmarks3d, predicted_images = self.latent2code(batch['shape_latent'].to(self.device), \
                                             batch['appearance_latent'].to(self.device), \
                                             batch['cam'].to(self.device), batch['pose'].to(self.device))
