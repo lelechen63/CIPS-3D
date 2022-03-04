065bcf614f07a09af79aa6cd233bf10ed71053e8
Lele Chen <lelechen@Innopeaks-MacBook-Pro-2.local>
2022-03-04 13:51:31 -0800

    remove

--- rignet/dataset.py
+++ rignet/dataset.py
@@ -79,7 +79,7 @@ class FFHQDataset(torch.utils.data.Dataset):
         _file.close()
 
         if opt.debug:
-            self.data_list = self.data_list[:513]
+            self.data_list = self.data_list[:100]
 
         _file = open(zip_path, "rb")
         self.total_data = pickle.load(_file)
