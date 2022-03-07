7c4a01787d674e1f5435fdd9aca477ad9cdb4267
Lele Chen <lelechen@Innopeaks-MacBook-Pro-2.local>
2022-03-07 11:02:51 -0800

    add lpss plot

--- utils/plot_loss.py
+++ utils/plot_loss.py
@@ -4,8 +4,13 @@ losstxt = '/home/uss00022/lelechen/github/CIPS-3D/checkpoints_debug3/Latent2Code
 reader = open(losstxt)
 l = reader.readline()
 ss = 0
+loss_land = []
+loss_tex = []
 while l:
     print (l)
+    tmp = l[-1].split(' ')
+    print (tmp)
+    loss_land.append(1)
     ss += 1
     if ss == 10:
         break
