c2f102ceb26e3aac6636cb563493dc8931a2d72d
Lele Chen <lelechen@Innopeaks-MacBook-Pro-2.local>
2022-03-29 18:28:46 -0700

    gg

--- photometric_optimization/reverse_image.py
+++ photometric_optimization/reverse_image.py
@@ -337,7 +337,7 @@ def main_ffhq_cips3d(config,start_idx =1):
     # image_path = "./test_images/69956.png"
     # img = imageio.imread(image_path)
 
-    config.savefolder = '/nfs/STG/CodecAvatar/lelechen/FFHQ/generated_cips3d/flame/'
+    config.savefolder = '/nfs/STG/CodecAvatar/lelechen/FFHQ/generated_cips3d/flame2/'
     
     k =  parse_args().k
     # gpuid = k % 7
@@ -350,7 +350,7 @@ def main_ffhq_cips3d(config,start_idx =1):
     for idx in tqdm(range(max(10000 * k,1 ),(k + 1) * 10000 )):
         try:
             img_p = os.path.join( root, 'images', '%d.png'%idx)
-            if not os.path.exists( config.savefolder + '/%d/flame_p.pickle'%idx):
+            # if not os.path.exists( config.savefolder + '/%d/flame_p.pickle'%idx):
                 os.makedirs(config.savefolder + '/%d'%idx, exist_ok = True)
                 img = cv2.imread(img_p)
                 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
