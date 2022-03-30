4fe5c528530625af6f18702de82b24d970d5bf4a
Lele Chen <lelechen@Innopeaks-MacBook-Pro-2.local>
2022-03-29 18:03:07 -0700

    gg

--- photometric_optimization/reverse_image.py
+++ photometric_optimization/reverse_image.py
@@ -399,7 +399,7 @@ def main_ffhq_stylenerf(config = config, parse = parse):
 
     root = '/nfs/STG/CodecAvatar/lelechen/FFHQ/generated_stylenerf'
     
-    for idx in tqdm(range(max(10000 * k,1 ),(k + 1) * 10000 )):
+    for idx in tqdm(range(max(10 * k,1 ),(k + 1) * 10 )):
         # if  idx > 166000 :
         #     continue
         # try:
